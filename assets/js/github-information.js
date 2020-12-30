3af776cb129ac643165bf9f2e4ed3f749437878b
Justin-Sawyer <justin.sawyer@icloud.com>
2020-12-30 19:32:09 +0000

    final commit

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -61,17 +61,18 @@ function fetchGitHubInformation(event) {
     ).then(
         function(firstResponse, secondResponse) {
             let userData = firstResponse[0];
+            //console.log(userData)
             let repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
+            console.log(errorResponse)
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
             } else if (errorResponse.status === 403) {
                 let resetTime = new Date(errorResponse.getResponseHeader(`X-RateLimit-Reset`)*1000);
                 $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
             } else {
-                console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error ${errorResponse.responseJSON.message}</h2>`
                 )
